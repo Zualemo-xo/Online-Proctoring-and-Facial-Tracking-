babf1d8145569d9df5e94f4a2af3fa27acfb423b
suriya-1403 <suriya@outlook.in>
2020-09-04 12:05:25 +0530

    Automated to open browser

--- mainexampage/main.py
+++ mainexampage/main.py
@@ -1,6 +1,9 @@
 
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+import webbrowser
+import sys
+import time
 
 app = Flask(__name__)
 
@@ -19,5 +22,21 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def main():
+    sites="website.txt"
+    browser ="safari"
+    web = webbrowser.get(browser)
+
+    with open(sites) as fobj:
+        try:
+            for num,url in enumerate(fobj):
+                web.open_new_tab(url.strip())
+                time.sleep(1)
+        except Exception as e:
+            print(e)
+
+
 if __name__ == '__main__':
+    main()
     app.run(host='0.0.0.0')
+    
